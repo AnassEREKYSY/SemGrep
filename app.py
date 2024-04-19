e8ec33ea3eba82294f8fca9851ef9aad5a10d898
AnassEREKYSY <ereanass@gmail.com>
2024-04-19 10:02:01 +0200

    First Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# Endpoint GET simple
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+# Endpoint POST avec une vulnérabilité d'injection de shell
+@app.route('/exec', methods=['POST'])
+def execute():
+    data = request.form.get('data')
+    eval(data) # Vulnérabilité ici : utilisation dangereuse de eval()
+    return 'ok'
+
+# Démarrage du serveur
+if __name__ == '__main__':
+    app.run(debug=True)
